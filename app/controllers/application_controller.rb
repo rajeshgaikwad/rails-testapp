b48f8eefae962a79704de18414af0358ae88f195
Rajesh Gaikwad <Rajesh.Gaikwad@gmail.com>
2013-07-27 20:16:17 +0530

    Fixing strong parmeters wayfor devise registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery
+
+   def configure_permitted_parameters
+         devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username,:name, :email,:password,:password_confirmation) }
+   end
 end
